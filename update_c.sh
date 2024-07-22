76c1bb7982c19351daddb10411e498ca3481f832
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2024-07-22 16:16:53 +0300

    Update update_c.sh

--- update_c.sh
+++ update_c.sh
@@ -11,18 +11,27 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+lsmod | grep -q ldt && ldt | grep -q tp0 && ldt rmdev tp0
+
 cd ympdccp
 #make M=net/core modules -j  $(nproc) || exit
 make M=net/dccp modules -j  $(nproc) || exit
 
 [ "$1" == "-x" ] && exit
 
+ssh 192.168.102.1 "{ \
+    /home/user/setdelay.sh 8 0 -q; \
+    /home/user/setdelay.sh 9 0 -q; \
+}"
+
 cp net/dccp/dccp.ko $dir/dccp/
 cp net/dccp/dccp_ipv4.ko $dir/dccp/
 cp net/dccp/mpdccp.ko $dir/dccp/
 cp net/dccp/mpdccplink.ko $dir/dccp/
 cp net/dccp/scheduler/mpdccp_sched_*.ko $dir/dccp/scheduler/
-cp net/dccp/non_gpl_scheduler/mpdccp_sched_*.ko $dir/dccp/scheduler/
+#cp net/dccp/reordering/*.ko $dir/dccp/reordering/
+cp net/dccp/non_gpl_reordering/*.ko $dir/dccp/non_gpl_reordering/
+cp net/dccp/non_gpl_scheduler/mpdccp_sched_*.ko $dir/dccp/non_gpl_scheduler/
 
 #cp net/core/devlink.ko $dir/core/
 #cp net/core/drop_monitor.ko $dir/core
@@ -32,27 +41,32 @@ cp net/dccp/non_gpl_scheduler/mpdccp_sched_*.ko $dir/dccp/scheduler/
 #scp net/core/drop_monitor.ko $ip_c:$dir/core
 #scp net/core/pktgen.ko $ip_c:$dir/core
 
+modprobe -r ldt
+modprobe -r mpdccp_reorder_active
 modprobe -r mpdccp
-modprobe mpdccp || exit
-echo 1 > /sys/module/mpdccp/parameters/mpdccp_debug
-echo 1 > /sys/module/dccp/parameters/dccp_debug
+#modprobe mpdccp || exit
+modprobe ldt
 
-#exit
+exit
+
+#echo 1 > /sys/module/mpdccp/parameters/mpdccp_debug
+#echo 1 > /sys/module/dccp/parameters/dccp_debug
+
+#echo active > /proc/sys/net/mpdccp/mpdccp_reordering
+#echo 3 > /sys/module/mpdccp/parameters/ro_dbug_state
+#echo 2 > /proc/sys/mpdccp_active_reordering/adaptive
+#echo 1 > /proc/sys/mpdccp_active_reordering/loss_detection
 
 scp net/dccp/dccp.ko $ip_c:$dir/dccp/
 scp net/dccp/dccp_ipv4.ko $ip_c:$dir/dccp/
 scp net/dccp/mpdccp.ko $ip_c:$dir/dccp/
 scp net/dccp/mpdccplink.ko $ip_c:$dir/dccp/
-scp net/dccp/scheduler/mpdccp_sched_*.ko $ip_c:$dir/dccp/scheduler/
-scp net/dccp/non_gpl_scheduler/mpdccp_sched_*.ko $ip_c:$dir/dccp/scheduler/
+scp net/dccp/scheduler/*.ko $ip_c:$dir/dccp/scheduler/
+scp net/dccp/non_gpl_scheduler/*.ko $ip_c:$dir/dccp/non_gpl_scheduler/
+scp net/dccp/non_gpl_reordering/*.ko $ip_c:$dir/dccp/non_gpl_reordering/
 
-ssh $ip_c " { \
-        modprobe -r mpdccp; \
-	modprobe mpdccp; \
-	echo 1 > /sys/module/mpdccp/parameters/mpdccp_debug; \
-	echo 1 > /sys/module/dccp/parameters/dccp_debug; \
-}"
-
-
+ssh $ip_c " { /home/user/refresh_mods; }"
+	#echo 1 > /sys/module/mpdccp/parameters/mpdccp_debug; \
+	#echo 1 > /sys/module/dccp/parameters/dccp_debug; \
 
 echo "replaced mpdccp modules"
