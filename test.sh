92bb61d5d6c8b48d9fa2f02100e7782dc73905bc
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2024-09-19 13:50:41 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,67 +1,81 @@
 #! /bin/bash
+
 ip_c="192.168.100.10"
 ip_r="192.168.102.1"
-iperfstr="iperf3 -c 10.0.42.1 -u -b 3000k -t 10 -4 -l 1050"
-#iperfstr="iperf3 -c 10.0.42.1 -t 10 -4"
+
+#iperfstr="iperf3 -c 10.0.42.1 -u -b 4000k -t 3 -4 -l 1050"
+iperfstr="iperf3 -c 10.0.42.1 -t 100 -4 -P 5"
+#iperfstr="iperf3 -c 192.168.102.10 -i 1 -4 --dccp --multipath -t 2 -b 3M"
+
+delay1=30
+delay2=10
 
 setup=0
 
-#echo default > /proc/sys/net/mpdccp/mpdccp_reordering
+ccid=2
+#nc -z -w 1 $ip_c 22 || exit
+
+/home/user/ccid$ccid.sh
+
+#echo default > /proc/sys/net/mpdccp/mpdccp_reordering #&& rmmod mpdccp_reorder_active
 echo active > /proc/sys/net/mpdccp/mpdccp_reordering
+echo 2 > /proc/sys/mpdccp_active_reordering/equalize_delay
+
 echo 0 > /proc/sys/mpdccp_active_reordering/adaptive
 echo 150 > /proc/sys/mpdccp_active_reordering/fixed_timeout
 echo 3 > /proc/sys/mpdccp_active_reordering/loss_detection
 echo 200 > /proc/sys/mpdccp_active_reordering/expiry_timeout
 echo 0 > /proc/sys/mpdccp_active_reordering/rtt_type
-echo 1 > /proc/sys/mpdccp_active_reordering/equalize_delay
+echo 2500 > /proc/sys/mpdccp_active_reordering/not_rcv_max
 
 
-#ssh $ip_r "tc qdisc add dev ifb0 root netem delay 30ms loss 3%"
-#ssh $ip_r "tc qdisc add dev ifb0 root netem delay 30ms loss gemodel 5 60 80 1"
+#ssh $ip_r "/home/user/ts.sh"
 
-ssh $ip_r "{ \
-    /home/user/setdelay.sh 8 30 -q; \
-    /home/user/setdelay.sh 9 10; \
+ssh $ip_c "{ \
+    /home/user/ccid$ccid.sh; \
+    echo rr > /proc/sys/net/mpdccp/mpdccp_scheduler; \
+    echo 0 > /proc/sys/net/mpdccp/mpdccp_rtt_config; \
+    echo 0 > /sys/module/mpdccp/parameters/ro_dbug_state; \
+    echo 0 > /sys/module/mpdccp/parameters/mpdccp_debug; \
+    echo 0 > /sys/module/dccp/parameters/dccp_debug; \
+    echo 0 > /proc/sys/net/ldt/debug; \
+    echo 0 > /sys/module/dccp/parameters/ccid2_debug; \
 }"
 
-#ssh $ip_r "tc qdisc add dev ifb0 root netem delay 30ms"
-#ssh $ip_r "tc qdisc add dev ifb1 root netem delay 10ms"
-#ssh $ip_r "tc qdisc add dev ifb1 root netem loss gemodel 5 60 80 1"
+#    ip l set enp0s9 down; \
+#    echo active > /proc/sys/net/mpdccp/mpdccp_reordering; \
 
-ssh $ip_c " { \
-    echo cubic > /proc/sys/net/ipv4/tcp_congestion_control; \
-    echo rr > /proc/sys/net/mpdccp/mpdccp_scheduler; \
-    echo 4 > /proc/sys/net/mpdccp/mpdccp_rtt_config; \
-    echo 0 > /sys/module/mpdccp/parameters/ro_dbug_state; \
-    echo 1 > /sys/module/mpdccp/parameters/mpdccp_debug; \
-    echo 1 > /sys/module/dccp/parameters/dccp_debug; \
-    echo 1 > /proc/sys/net/ldt/debug; \
-    echo 1 > /sys/module/dccp/parameters/ccid2_debug; \
-    dmesg -C; \
+ssh $ip_r "{ \
+    /home/user/setdelay.sh 8 $delay1 -q; \
+    /home/user/setdelay.sh 9 $delay2; \
+    /home/user/tcheck.sh s; \
 }"
 
 [ "$(ldt | grep tp0)" ] || ./tunsetup.sh
 
+sleep 1
 echo 0 > /sys/module/mpdccp/parameters/ro_dbug_state
 echo 0 > /sys/module/mpdccp/parameters/mpdccp_debug
 echo 0 > /sys/module/dccp/parameters/dccp_debug
 echo 0 > /proc/sys/net/ldt/debug
 
 
-dmesg -C
-#ssh $ip_c "/home/user/tunsetup.sh && sleep 0.5"
-#ssh $ip_c "/home/user/tunsetup.sh && sleep 0.5 && ping 10.0.42.1 -c 6 && sleep 0.5"
-ssh $ip_c "/home/user/tunsetup.sh && sleep 0.5"
-ssh $ip_c "dmesg" | grep -q "Failed to set up MPDCCP Client mpcb" && setup=failed
+ssh $ip_c '{ \
+    /home/user/tunsetup.sh && sleep 1; \
+    dmesg | grep -q "Failed to set up MPDCCP Client mpcb" && setup=failed; \
+    echo "Testing $(</proc/sys/net/mpdccp/mpdccp_scheduler) scheduler enp8: $(</sys/module/mpdccplink/links/dev/enp0s8/mpdccp_prio) enp9: $(</sys/module/mpdccplink/links/dev/enp0s9/mpdccp_prio)"; \
+}'
+#    dmesg -C; \
 
+#read
+
+dmesg -C
 if [ $setup != "failed" ]; then
 	iperf3 -s -i 1 -4 &
 	#tcpdump -i tp0 -q udp port 5201 -tt -l -x > /home/user/tclogp &
 	sleep 0.5
 
 	ssh $ip_c $iperfstr > /dev/null 2>&1 && sleep 1
-##	ssh $ip_c "iperf3 -c 10.0.42.1 -u -b 4M -t 10 -4 -l 1050 && sleep 1"
-
 	#pkill tcpdump
 	pkill iperf3
 
@@ -71,7 +85,6 @@ if [ $setup != "failed" ]; then
 else
 	echo "setup failed, ldt module bricked"
 fi
-
 
 ssh $ip_c "ldt rmdev tp0"
 
@@ -89,12 +102,16 @@ sleep 2 && ss -d | grep -q "192.168.102.10" && echo "socket still active" &
 
 
 echo $iperfstr
+ssh $ip_r /home/user/tcheck.sh r
 
 
 #ssting=$(dmesg | grep "send 100" | tail -1 | grep -o 'DEQ(\w*): send')
 #rstring=$(dmesg | grep "receive 100" | tail -1 | grep -o 'DEQ(\w*)')
 rstring=$(dmesg | grep -m1 "receive [0-9]*99" | grep -o 'DEQ(\w*)')
 dmesg | grep -e "$rstring" > /tmp/mptest.log
+[ $(wc -l < /tmp/mptest.log) -lt 30 ] && exit
+
+
 python3 /home/user/Documents/mpplot/line.py &
 python3 /home/user/Documents/mpplot/owd.py &
 
