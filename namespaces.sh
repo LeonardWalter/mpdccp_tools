f0722f26f56a133999b78668d34a28055400adac
Leonard Walter <50204487+LeonardWalter@users.noreply.github.com>
2024-09-30 20:21:32 +0300

    Update namespaces.sh

--- namespaces.sh
+++ namespaces.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 
-
 ##### TEST CONFIGURATION #####
 
 dmesg -C
 
 #iperfstr="iperf3 -c 10.0.42.1 -u -b 3000k -t 10 -4 -l 1050"
 iperfstr="iperf3 -c 10.0.42.1 -t 100 -4" # -P 5"
-#iperfstr="iperf3 -c 192.168.102.10 -t 10 -4"
+#iperfstr="iperf3 -c 192.168.102.10 -t 100 -4"
+
+#ns1="1"
 
 delay1=30
 delay2=10
 
-/home/user/ccid5.sh
+/home/user/ccid.sh 2
 
 echo 0 > /sys/module/mpdccp/parameters/ro_dbug_state
 echo 0 > /sys/module/dccp/parameters/ccid2_debug
@@ -21,7 +22,7 @@ echo 1 > /sys/module/mpdccp/parameters/mpdccp_debug
 
 #echo default > /proc/sys/net/mpdccp/mpdccp_reordering && rmmod mpdccp_reorder_active
 echo active > /proc/sys/net/mpdccp/mpdccp_reordering
-echo 0 > /proc/sys/mpdccp_active_reordering/equalize_delay
+echo 2 > /proc/sys/mpdccp_active_reordering/equalize_delay
 
 echo 0 > /proc/sys/mpdccp_active_reordering/adaptive
 echo 150 > /proc/sys/mpdccp_active_reordering/fixed_timeout
@@ -41,6 +42,13 @@ server_setup () {
 	ip r add default via 192.168.102.1 dev enp0s8
 }
 export -f server_setup
+
+plot () {
+        python3 /home/user/Documents/mpplot/delayplot.py &
+        python3 /home/user/Documents/mpplot/line.py &
+        python3 /home/user/Documents/mpplot/owd.py &
+        exit
+}
 
 client_setup () {
 	ip a add 192.168.$2.10/24 dev $1
@@ -78,18 +86,21 @@ client_tun () {
 export -f client_tun
 
 ns_setup () {
-	ip netns add ns1
-	ip l set enp0s9 netns ns1
-	ip netns exec ns1 bash -c 'client_setup "enp0s9" "100" "5"'
+	if [ -n "$ns1" ]; then
+		ip netns add ns1
+		ip l set enp0s9 netns ns1
+		ip l set enp0s10 netns ns1
+	fi
 
-	ip l set enp0s10 netns ns1
-	ip netns exec ns1 bash -c 'client_setup "enp0s10" "101" "4"'
+	$ns1 bash -c 'client_setup "enp0s9" "100" "5"'
+	$ns1 bash -c 'client_setup "enp0s10" "101" "4"'
 
 	ip netns add ns2
 	ip l set enp0s8 netns ns2
 	ip netns exec ns2 bash -c 'server_setup'
 	sleep 1
 	echo "namespaces configured"
+	sleep 1
 }
 
 tun_setup () {
@@ -97,15 +108,15 @@ tun_setup () {
 	sleep 2
 	ip netns exec ns2 bash -c 'server_tun'
 	sleep 2
-	ip netns exec ns1 bash -c 'client_tun'
+	$ns1 bash -c 'client_tun'
 	echo "mpdccp tunnels configured"
 }
 
 reset () {
-	ip netns exec ns1 ldt rmdev tp1
+	$ns1 ldt rmdev tp1
 	sleep 1
 	ip netns exec ns2 ldt rmdev tp0
-	sleep 2
+	sleep 1
 	modprobe -r ldt
 	modprobe -r mpdccp_reorder_active
 	modprobe -r mpdccp_sched_rr
@@ -114,12 +125,14 @@ reset () {
 }
 
 [ "$1" == "-x" ] && reset
-[ "$(ip netns list | grep ns1)" ] || ns_setup
+[ "$1" == "-p" ] && plot
+[ -n "$ns1" ] && ns1="ip netns exec ns1"
+[ "$(ip netns list | grep ns2)" ] || ns_setup
 [ "$(ip netns exec ns2 ldt | grep tp0)" ] || tun_setup
 
-ip netns exec ns1 bash -c 'tc -s qdisc show dev enp0s9 && tc -s qdisc show dev enp0s10'
+$ns1 bash -c 'tc -s qdisc show dev enp0s9 && tc -s qdisc show dev enp0s10'
 echo " "
-echo "Testing $(</proc/sys/net/mpdccp/mpdccp_scheduler) scheduler enp9: $(ip netns exec ns1 cat /sys/module/mpdccplink/links/dev/enp0s9/mpdccp_prio) enp10: $(ip netns exec ns1 cat /sys/module/mpdccplink/links/dev/enp0s10/mpdccp_prio)"
+echo "Testing $(</proc/sys/net/mpdccp/mpdccp_scheduler) scheduler enp9: $($ns1 cat /sys/module/mpdccplink/links/dev/enp0s9/mpdccp_prio) enp10: $($ns1 cat /sys/module/mpdccplink/links/dev/enp0s10/mpdccp_prio)"
 
 ip netns exec ns2 ssh 192.168.102.1 "{ \
     /home/user/setdelay.sh 8 $delay1 -q; \
@@ -135,11 +148,12 @@ read
 echo 0 > /sys/module/dccp/parameters/dccp_debug
 echo 0 > /sys/module/mpdccp/parameters/mpdccp_debug
 
-ip netns exec ns1 $iperfstr > /dev/null 2>&1
+$ns1 $iperfstr > /dev/null 2>&1
 
 ip netns exec ns2 ssh 192.168.102.1 /home/user/tcheck.sh r
 sleep 1
 ip netns exec ns2 pkill iperf3 > /dev/null 2>&1
+
 
 server=$(dmesg | grep -m1 "role 3" | grep -oP '\(\K[^\)]+')
 client=$(dmesg | grep -m1 "role 2" | grep -oP '\(\K[^\)]+')
@@ -149,9 +163,5 @@ dstring="DEQ($server): delaying"
 fstring="DEQ($server): forward"
 
 dmesg | grep -e "$sstring" -e "$rstring" -e "$dstring" -e "$fstring" > /tmp/mptest.log
-
-python3 /home/user/Documents/mpplot/delayplot.py &
-python3 /home/user/Documents/mpplot/line.py &
-python3 /home/user/Documents/mpplot/owd.py &
-
 echo "Packet count - rx: $(dmesg | grep "$rstring" | wc -l) delay: $(dmesg | grep "$dstring" | wc -l)"
+plot
